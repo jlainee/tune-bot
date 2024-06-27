d2e5c73a91e67572e78de88d0a467ff8d46181d9
Jere Laine <93613037+jlainee@users.noreply.github.com>
2024-06-27 22:37:52 +0300

    Add signalHandler.ts
    - SIGINT and SIGTERM handler implemented.

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,12 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import { config } from "./config";
 import handleReady from "./events/ready";
+import handleSignals from "./utils/signalHandler";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 client.once(Events.ClientReady, (client: Client) => handleReady(client));
 
 // client.login(config.DISCORD_TOKEN);
+
+handleSignals();
